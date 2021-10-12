69167f5fa713995bbd1290930b63ec8b1da6c4d4
Bibek Rai <bibekrai655@gmail.com>
2021-10-12 07:41:40 +0545

    .

---
+++ Python Program to Illustrate Different Set Operations.py
@@ -0,0 +1,6 @@
+a={0,2,4,6}
+b={1,3,5,7}
+print("The union of a and b is",a|b)
+print("The intersection of a and b is",a & b)
+print("The difference of a and b is",a-b)
+print("The symmetric difference of a and b is",a^b)
